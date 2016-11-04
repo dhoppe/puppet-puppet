45e4cc4df455753f880bf95ed3e14e50db1d20ee
Dennis Hoppe <github@debian-solutions.de>
2016-11-04 21:18:57 +0100

    Fix several Rubocop issues

--- spec/defines/define_spec.rb
+++ spec/defines/define_spec.rb
@@ -1,77 +1,87 @@
 require 'spec_helper'
 
-describe 'puppet::define', :type => :define do
+describe 'puppet::define', type: :define do
   ['Debian'].each do |osfamily|
-    let(:facts) {{
-      :osfamily => osfamily,
-    }}
+    let(:facts) do
+      {
+        osfamily: osfamily
+      }
+    end
     let(:pre_condition) { 'include puppet' }
     let(:title) { 'puppet.conf' }
 
     context "on #{osfamily}" do
       context 'when source file' do
-        let(:params) {{
-          :config_file_path   => '/etc/puppet/puppet.2nd.conf',
-          :config_file_source => 'puppet:///modules/puppet/common/etc/puppet/puppet.conf',
-        }}
+        let(:params) do
+          {
+            config_file_path: '/etc/puppet/puppet.2nd.conf',
+            config_file_source: 'puppet:///modules/puppet/common/etc/puppet/puppet.conf'
+          }
+        end
 
         it do
           is_expected.to contain_file('define_puppet.conf').with(
             'ensure'  => 'present',
             'source'  => 'puppet:///modules/puppet/common/etc/puppet/puppet.conf',
             'notify'  => 'Service[puppet]',
-            'require' => 'Package[puppet]',
+            'require' => 'Package[puppet]'
           )
         end
       end
 
       context 'when content string' do
-        let(:params) {{
-          :config_file_path   => '/etc/puppet/puppet.3rd.conf',
-          :config_file_string => '# THIS FILE IS MANAGED BY PUPPET',
-        }}
+        let(:params) do
+          {
+            config_file_path: '/etc/puppet/puppet.3rd.conf',
+            config_file_string: '# THIS FILE IS MANAGED BY PUPPET'
+          }
+        end
 
         it do
           is_expected.to contain_file('define_puppet.conf').with(
             'ensure'  => 'present',
-            'content' => /THIS FILE IS MANAGED BY PUPPET/,
+            'content' => %r{THIS FILE IS MANAGED BY PUPPET},
             'notify'  => 'Service[puppet]',
-            'require' => 'Package[puppet]',
+            'require' => 'Package[puppet]'
           )
         end
       end
 
       context 'when content template' do
-        let(:params) {{
-          :config_file_path     => '/etc/puppet/puppet.4th.conf',
-          :config_file_template => 'puppet/common/etc/puppet/puppet.conf.erb',
-        }}
+        let(:params) do
+          {
+            config_file_path: '/etc/puppet/puppet.4th.conf',
+            config_file_template: 'puppet/common/etc/puppet/puppet.conf.erb'
+          }
+        end
 
         it do
           is_expected.to contain_file('define_puppet.conf').with(
             'ensure'  => 'present',
-            'content' => /THIS FILE IS MANAGED BY PUPPET/,
+            'content' => %r{THIS FILE IS MANAGED BY PUPPET},
             'notify'  => 'Service[puppet]',
-            'require' => 'Package[puppet]',
+            'require' => 'Package[puppet]'
           )
         end
       end
 
       context 'when content template (custom)' do
-        let(:params) {{
-          :config_file_path         => '/etc/puppet/puppet.5th.conf',
-          :config_file_template     => 'puppet/common/etc/puppet/puppet.conf.erb',
-          :config_file_options_hash => {
-            'key' => 'value',
-          },
-        }}
+        let(:params) do
+          {
+            config_file_path: '/etc/puppet/puppet.5th.conf',
+            config_file_template: 'puppet/common/etc/puppet/puppet.conf.erb',
+            config_file_options_hash: {
+              'key' => 'value'
+            }
+          }
+        end
 
         it do
           is_expected.to contain_file('define_puppet.conf').with(
             'ensure'  => 'present',
-            'content' => /THIS FILE IS MANAGED BY PUPPET/,
+            'content' => %r{THIS FILE IS MANAGED BY PUPPET},
             'notify'  => 'Service[puppet]',
-            'require' => 'Package[puppet]',
+            'require' => 'Package[puppet]'
           )
         end
       end
